698d59e8d9096c0c1aafabdf1228eb995e499256
Jeff Wymore <yardboy2401@gmail.com>
2021-12-15 19:36:06 -0800

    server file cleanup and comments added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
+//require express
 const express = require('express');
+
+//require routes folder
 const routes = require('./routes');
+
 // import sequelize connection
 const sequelize = require('./config/connection');
 
+//create instance of express for app variable
 const app = express();
+
+//declaring PORT for live deployment or localhost PORT number
 const PORT = process.env.PORT || 3001;
 
+//express use of json format
 app.use(express.json());
+
+//express middleware
 app.use(express.urlencoded({ extended: true }));
 
+//express middleware for routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
